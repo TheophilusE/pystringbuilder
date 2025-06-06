a5fe19668a61ce69a8cd21c9514094dae640049a
Theophilus Eriata <theoeriata@gmail.com>
2025-06-06 10:16:24 -0600

    Add string builder unit test.

---
+++ tests/stringbuildertest.py
@@ -0,0 +1,57 @@
+import unittest
+
+from pystringbuilder.stringbuilder import StringBuilder
+
+class TestStringBuilder(unittest.TestCase):
+    def test_append(self):
+        sb = StringBuilder()
+        sb.append("Hello")
+        self.assertEqual(str(sb), "Hello")
+
+    def test_append_line(self):
+        sb = StringBuilder()
+        sb.append_line("Hello")
+        self.assertEqual(str(sb), "Hello\n")
+
+    def test_insert(self):
+        sb = StringBuilder()
+        sb.append("Hello World")
+        sb.insert(6, "Beautiful ")
+        self.assertEqual(str(sb), "Hello Beautiful World")
+
+    def test_delete(self):
+        sb = StringBuilder()
+        sb.append("Hello Beautiful World")
+        sb.delete(6, 16)
+        self.assertEqual(str(sb), "Hello World")
+
+    def test_replace(self):
+        sb = StringBuilder()
+        sb.append("Hello World")
+        sb.replace("World", "Universe")
+        self.assertEqual(str(sb), "Hello Universe")
+
+    def test_reverse(self):
+        sb = StringBuilder()
+        sb.append("Python")
+        sb.reverse()
+        self.assertEqual(str(sb), "nohtyP")
+
+    def test_substring(self):
+        sb = StringBuilder()
+        sb.append("Hello World")
+        self.assertEqual(sb.substring(6, 11), "World")
+
+    def test_find(self):
+        sb = StringBuilder()
+        sb.append("Hello World")
+        self.assertEqual(sb.find("World"), 6)
+
+    def test_clear(self):
+        sb = StringBuilder()
+        sb.append("Hello")
+        sb.clear()
+        self.assertEqual(str(sb), "")
+
+if __name__ == "__main__":
+    unittest.main()
